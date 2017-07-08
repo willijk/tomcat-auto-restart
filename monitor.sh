e6778eb7df2bd8d0fcaf34354c7cb64f859d5fa4
bowei <bowei_chan@163.com>
2017-07-08 15:37:27 +0800

    add the core monitor script

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log=./monitor.log
 monitor(){
-    url=http://localhost:8080/upload/test
+    url=http://localhost:8080/test
     success=$(curl -s -o /dev/null -m 10 -w %{http_code} ${url})
     if [ 200 -eq $success ];then
         echo "[INFO `date`] tomcat already succes started"
