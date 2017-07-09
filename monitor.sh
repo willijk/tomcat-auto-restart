c6469cd2eec4badad50dbd7987d6ca78b3ee8179
bowei <bowei_chan@163.com>
2017-07-09 21:37:05 +0800

    split the log file by hour

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-export CATALINA_HOME=/usr/local/tomcat-7
-log=$CATALINA_HOME/logs/monitor.log
+export CATALINA_HOME=/usr/local/tomcat
+date=$(date +%F-%H)
+log=$CATALINA_HOME/logs/monitor/$date.log
+ls $CATALINA_HOME/logs/monitor > /dev/null || mkdir $CATALINA_HOME/logs/monitor && touch $log
 monitor(){
     url=http://localhost:8080/test
     success=$(curl -s -o /dev/null -m 10 -w %{http_code} ${url})
-    if [ 200 -eq $success ];then
+    if [ 200 -eq "$success" ];then
         echo "[INFO `date`] tomcat already succes started"
     else
         echo "[INFO `date`] tomcat collapse"
