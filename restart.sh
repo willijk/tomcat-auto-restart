db8dd5f06ec54a6de6e655a0b275362cbe184ea7
will-chance <bowei_chan@163.com>
2017-07-09 15:46:51 +0800

    Update restart.sh
    quote the variable ${tomcatPid} to fix the issue #1

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ tomcatPid=`ps -ef | grep tomcat | grep -v grep | awk '{print $2}'`
 export JRE_HOME=/usr/java/jdk1.7.0_80/jre
 export CATALINA_HOME=/usr/local/tomcat-7
 log=$CATALINA_HOME/logs/monitor.log
-if [ -n ${tomcatPid} ];then
+if [ -n "${tomcatPid}" ];then
     kill -9 $tomcatPid
     sleep 2
     $CATALINA_HOME/bin/startup.sh 
